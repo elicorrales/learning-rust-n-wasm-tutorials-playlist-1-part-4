2388ff317a33f94d008459a8b0db7f270cf3550c
Eli Corrales <e_corrales2003@yahoo.com>
2022-09-04 21:22:50 -0400

    .

---
+++ my-first-wasm-bin/src/main.rs
@@ -0,0 +1,51 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    println!("Main(): Hello, world!");
+}
+
+#[no_mangle]
+pub fn say_hello() {
+    println!("Say Hello World!");
+}
+
+#[no_mangle]
+pub fn say_hello_2() -> String {
+    "Return Hello World!".to_string()
+}
+
+#[no_mangle]
+pub fn echo(message: String) -> String {
+    "Echo ".to_string() + &message
+}
+
+#[no_mangle]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[no_mangle]
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[no_mangle]
+pub fn square(a: i32) -> i32 {
+    a * a
+}
+
+#[no_mangle]
+pub fn loop_hello() {
+    for n in 0..5 {
+        println!("{} Hello World!", n);
+    }
+}
+
+#[no_mangle]
+pub fn delay_loop_hello() {
+    for n in 0..5 {
+        println!("{} Hello World!", n);
+        sleep(Duration::from_secs(1));
+    }
+}
